0fca9531bf52a42746bdb3b63230abf4d8bb6289
g.song@cinqprobe.com <g.song@cinqprobe.com>
2025-06-06 17:59:51 +0900

    add4

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ class Main{
 	public static void main(String args[]){
 
 	   System.out.println("1111");
-	   System.out.println("2");
+	   System.out.println("22");
+System.out.println("3");
 
 	}
 }
